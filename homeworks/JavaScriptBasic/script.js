fe09d94851c8dabc75e3072df0fcb8b79efd5508
MartinLutovski <martinlutovski@gmail.com>
2024-12-04 00:52:37 +0100

    syntax

--- homeworks/JavaScriptBasic/script.js
+++ homeworks/JavaScriptBasic/script.js
@@ -1,14 +1,14 @@
 // console.log("isitworking")
 
-let phone = 119.95
-let tax = 5
-let percentage = 100
-let onepercent = phone / percentage
-let phoneplustax = phone + onepercent * tax
-let totalprice = 30
-let display = phoneplustax * totalprice
+let phone = 119.95;
+let tax = 5;
+let percentage = 100;
+let onepercent = phone / percentage;
+let phoneplustax = phone + onepercent * tax;
+let totalprice = 30;
+let display = phoneplustax * totalprice;
 
-console.log(display)
+console.log(display);
 
 
-document.getElementById("phoneplustaxmultiplyedby30").innerText = display
+document.getElementById("phoneplustaxmultiplyedby30").innerText = display;

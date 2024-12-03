c242be5d322b238eb76737a880c3ea625f954764
MartinLutovski <martinlutovski@gmail.com>
2024-12-03 20:59:03 +0100

    Add files via upload

---
+++ random code/kod od cas/JavaScriptBasic/03-12-24-javabasic/scripts/exercises.js
@@ -0,0 +1,35 @@
+// konverzija
+let feet = 100;
+let meters = 0.3048;
+
+let result = feet * meters;
+
+console.log("meters", result);
+
+// plostina
+let sideA = 20;
+let sideB = 30;
+
+let area = sideA * sideB;
+
+console.log("The area of the rectangle is:", area);
+
+// bad example bellow- name them GOOD!
+
+let a = 20;
+let b = 30;
+let c = a * b;
+
+console.log(c);
+
+// plostina
+
+let p = 3.14;
+let r = 5;
+
+let area_circle = p * r * r;
+console.log("Plostina e :", area_circle);
+
+// Cannot declare same name variable twice
+// let test= 10
+// let test= 20
---
+++ random code/kod od cas/JavaScriptBasic/03-12-24-javabasic/scripts/variables.js
@@ -0,0 +1,59 @@
+// alert ("hello from variables.js")
+
+// Declare and initialize variable
+
+// Numbers
+let firstNumber;
+let secondNumber = 10;
+let thirdNumber = 1125;
+let decimalNumber = 11.28;
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+console.log(typeof firstNumber);
+console.log(typeof secondNumber);
+console.log(typeof decimalNumber);
+
+let firstName = "Martin";
+let lastName = "Panovski";
+let message = "hello from sedc";
+
+let numericString = "31";
+console.log("the message sent is:");
+console.log(message);
+
+console.log(firstName);
+console.log(lastName);
+
+console.log(firstName);
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof numericString);
+
+let isRainingOutside = false;
+let isColdOutside = true;
+console.log("today is raining!");
+console.log("Sorry but your statment is:");
+console.log(isRainingOutside);
+console.log(isColdOutside);
+console.log(typeof isRainingOutside);
+console.log(typeof isColdOutside);
+
+// variajablata ja message ja menuvame vo 19
+
+message = 19;
+console.log("the message sent again is:");
+console.log(message);
+
+let newLine = "this is the first line. \nThis is the secondline!";
+console.log(newLine)
+
+
+/* If i want
+to comment in 
+multiple lines */
+
+
+let nullableVariable = null;
+console.log(nullableVariable);

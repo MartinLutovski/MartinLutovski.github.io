23c20fdf38bb7393a92b4454e18c3fa9d2e17bcd
MartinLutovski <martinlutovski@gmail.com>
2024-12-08 02:28:52 +0100

    linking

--- homeworks/JavaScriptBasic/ATM/atm.js
+++ homeworks/JavaScriptBasic/ATM/atm.js
@@ -1,14 +1,16 @@
-let balance = 10000
+let balance = 10000;
 
-let amount = Number(prompt(`You have ${balance}! Enter the amout you want to withdraw:`));
+let amount = Number(
+  prompt(`You have ${balance}! Enter the amout you want to withdraw:`)
+);
 
 function atm(amount) {
-    if (amount <= balance) {
-        balance -= amount;
-        return `You withdrew: ${amount}. Remaining balance: ${balance}`
-    } else {
-        return "Not enough balance.";
-    }
+  if (amount <= balance) {
+    balance -= amount;
+    return `You withdrew: ${amount}. Remaining balance: ${balance}`;
+  } else {
+    return "Not enough balance.";
+  }
 }
 
 alert(atm(amount));
--- homeworks/JavaScriptBasic/typeof/typeof.js
+++ homeworks/JavaScriptBasic/typeof/typeof.js
@@ -1,14 +1,11 @@
 function getType(input) {
-    let type = typeof input; 
-    console.log(`The type of the input is: ${type}`); 
-    return type; 
-  }
-  
-  
-  getType(Infinity); // Number
-  getType("random text"); // String
-  getType(true); // Boolean
-  getType({}); // Object
-  getType(); // Undefined
-  
-  
+  let type = typeof input;
+  console.log(`The type of the input is: ${type}`);
+  return type;
+}
+
+getType(Infinity); // Number
+getType("random text"); // String
+getType(true); // Boolean
+getType({}); // Object
+getType(); // Undefined

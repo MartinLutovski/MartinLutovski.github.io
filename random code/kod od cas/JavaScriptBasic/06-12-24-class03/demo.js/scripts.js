417478f937d4a6d991dedf402a94d56a38028baa
MartinLutovski <martinlutovski@gmail.com>
2024-12-11 03:56:09 +0100

    folders
    folders

---
+++ random code/kod od cas/JavaScriptBasic/06-12-24-class03/demo.js/scripts.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+}
+else if(a < b){
+    console.log(`${a} is lower than ${b}`);
+
+    
+} 
+else {
+  console.log(`${a} is equal to ${b}`);
+}
+
+// flow if else, and execution line by line

ed9a7b4c0c0f69c24cf16b69e9574fd0dbef6766
MartinLutovski <martinlutovski@gmail.com>
2024-12-20 00:51:47 +0100

    prompts outside of objects on objectAnimal & readingStatus

--- homeworks/JavaScriptBasic/objectAnimal/objectAnimal.js
+++ homeworks/JavaScriptBasic/objectAnimal/objectAnimal.js
@@ -19,13 +19,33 @@
 
 // Now with prompt
 
-let animal = {
-  name: prompt("Enter the animal's name:"),
-  kind: prompt("Enter the kind of animal:"),
+// let animal = {
+//   name: prompt("Enter the animal's name:"),
+//   kind: prompt("Enter the kind of animal:"),
 
-  speak: function (message) { if (animal.name )
-    alert(`${this.name} the ${this.kind} says: '${message}'`);
+//   speak: function (message) { if (animal.name )
+//     alert(`${this.name} the ${this.kind} says: '${message}'`);
+//   },
+// };
+
+// animal.speak(prompt("What do you want the animal to say?"));   ================= prompt should be outside object, in order for the object to be more modular.
+
+// prompt outside bellow
+
+let animal = {
+  name: "",
+  kind: "",
+
+  speak: function (message) {
+    if (this.name && this.kind) {
+      alert(`${this.name} the ${this.kind} says: '${message}'`);
+    } else {
+      alert("Please enter animal name and kind first!");
+    }
   },
 };
+
+animal.name = prompt("Enter the animal's name:");
+animal.kind = prompt("Enter the kind of animal:");
 
 animal.speak(prompt("What do you want the animal to say?"));
--- homeworks/JavaScriptBasic/readingStatus/readingStatus.js
+++ homeworks/JavaScriptBasic/readingStatus/readingStatus.js
@@ -21,6 +21,32 @@
 // console.log(book.displayStatus());
 
 // Now with prompt
+// function validateInput(input) {
+//   while (input === null || input.trim() === "") {
+//     input = prompt("Invalid input! Please enter a valid value:");
+//   }
+//   return input.trim();
+// } // Note to self: doesnt work if it isn't declared above.
+
+// let book = {
+//   title: validateInput(prompt("Enter the book title:")),
+//   author: validateInput(prompt("Enter the author's name:")),
+//   readingStatus: confirm(
+//     "Have you read this book? Click OK for yes, Cancel for no."
+//   ), // tnx Google, prasanje: ova e object vo JS?
+
+//   displayStatus: function () {
+//     if (this.readingStatus) {
+//       return `Already read '${this.title}' by ${this.author}.`;
+//     } else {
+//       return `You still need to read '${this.title}' by ${this.author}.`;
+//     }
+//   },
+// };
+
+// alert(book.displayStatus());    as with object animal, prompts should be outside object/method, in order to achieve better modularity.
+
+// bellow with prompt outside
 function validateInput(input) {
   while (input === null || input.trim() === "") {
     input = prompt("Invalid input! Please enter a valid value:");
@@ -29,11 +55,9 @@ function validateInput(input) {
 } // Note to self: doesnt work if it isn't declared above.
 
 let book = {
-  title: validateInput(prompt("Enter the book title:")),
-  author: validateInput(prompt("Enter the author's name:")),
-  readingStatus: confirm(
-    "Have you read this book? Click OK for yes, Cancel for no."
-  ), // tnx Google, prasanje: ova e object vo JS?
+  title: "",
+  author: "",
+  readingStatus: "",
 
   displayStatus: function () {
     if (this.readingStatus) {
@@ -44,4 +68,9 @@ let book = {
   },
 };
 
-alert(book.displayStatus());
+book.title = validateInput(prompt("Enter the book title:"));
+book.author = validateInput(prompt("Enter the author's name:"));
+(book.readingStatus = confirm(
+  "Have you read this book? Click OK for yes, Cancel for no."
+)), // tnx Google, prasanje: ova e object vo JS?
+  alert(book.displayStatus());

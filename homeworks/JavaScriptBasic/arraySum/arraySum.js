f6b942d28e7648a589636ca1714e88d165f47923
MartinLutovski <martinlutovski@gmail.com>
2024-12-23 01:31:15 +0100

    arraySum + linking

---
+++ homeworks/JavaScriptBasic/arraySum/arraySum.js
@@ -0,0 +1,26 @@
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let numberList = document.getElementById("numberList");
+let sumParagraph = document.getElementById("sum");
+let equationParagraph = document.getElementById("equation");
+
+let sum = 0;
+let equation = "";
+
+numbersArray.forEach((num, index) => {
+  let listNumber = document.createElement("li");
+  listNumber.innerText = num;
+  numberList.appendChild(listNumber); // add child element, as last child.
+  sum += num;
+  equation += num;
+  if (index < numbersArray.length - 1) {
+    //-1 to remove the last "+"
+    equation += " + ";
+  }
+});
+console.log(sum);
+console.log(equation);
+
+equation += ` = ${sum}`;
+sumParagraph.innerText = `Sum: ${sum}`;
+equationParagraph.innerText = `Equation: ${equation}`;
